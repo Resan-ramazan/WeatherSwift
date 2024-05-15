46a93bee98aaa812eb994f63471473df9326e6af
Ramazan Alkoyun <ramazan.resan@gmail.com>
2024-05-15 14:09:32 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by RESAN on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
